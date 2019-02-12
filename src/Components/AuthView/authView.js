cc50ce2a74398851b06c74f3f4d3754ec05bd38e
Kara Rainey <karain82@hotmail.com>
2019-02-12 12:26:03 -0600

    server is working

---
+++ src/Components/AuthView/authView.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+export default class AuthView extends Component{
+
+    render(){
+        return(
+            <div className="auth_main">
+                <div className="auth_main_container">
+                    <p className="auth_icon">:)</p>
+                    <h2 className="auth_title">Helo</h2>
+                    <button className="auth_button">Login / Register</button>
+                </div>
+            </div>
+        )
+    }
+}
